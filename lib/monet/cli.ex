2f88c31db98122ee5c624524c187f762c24ec9a9
Madhu Srinivasan <msrinivasa@cray.com>
2021-08-21 00:39:10 -0700

    added releases

--- lib/monet/cli.ex
+++ lib/monet/cli.ex
@@ -8,16 +8,19 @@ defmodule Monet.CLI do
   defp parse_args(args) do
     {opts, _, _} =
       args
-      |> OptionParser.parse(strict: [host: :string])
+      |> OptionParser.parse(strict: [host: :string, cookie: :string])
     opts
   end
 
   defp execute(opts) do
-    IO.puts opts[:host]
+    IO.inspect opts[:cookie]
+    # set the node name and cookie
     Monet.SimpleQueue.enqueue(20)
     IO.inspect Monet.SimpleQueue.queue
     IO.inspect Monet.SimpleQueue.dequeue
     IO.inspect Monet.SimpleQueue.queue
   end
- 
 end
+
+# reference for setting nodenames: https://stackoverflow.com/questions/41269876/elixir-set-cookie-and-hostname-for-release-created-using-mix-escript-build
+# reference for no halting: https://stackoverflow.com/questions/30073563/how-can-i-build-an-elixir-escript-that-does-not-halt-the-erlang-vm-after-executi
